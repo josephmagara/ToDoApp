92ba6a09e559718db513a1cdc33eb4e4fe1d511a
Joseph Magara <joseph.magara@growthops.com.au>
2018-09-13 11:55:05 +1000

    Starting the project

---
+++ app/src/main/java/com/example/josephmagara/todoapp/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.example.josephmagara.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+  }
+}
