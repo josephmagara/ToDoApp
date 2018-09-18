923f1e0e6369cffe02f9783e643d458d3227c411
Joseph Magara <joseph.magara@growthops.com.au>
2018-09-18 14:54:34 +1000

    Added a query that will get todos that were created in a specific time range

--- app/src/main/java/org/josephmagara/todo/android/activities/android/data/interfaces/UserToDoDoa.kt
+++ app/src/main/java/org/josephmagara/todo/android/activities/android/data/interfaces/UserToDoDoa.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
 import org.josephmagara.todo.android.activities.android.data.models.UserToDo
+import java.util.Date
 
 
 @Dao
@@ -14,20 +15,21 @@ interface UserToDoDoa{
     private const val SELECT_ALL = "SELECT * FROM userToDo"
     private const val SELECT_BY_ID = "SELECT * FROM userToDo WHERE id IN (:toDoIds)"
     private const val SELECT_BY_NAME = "SELECT * FROM userToDo WHERE title LIKE :first LIMIT 1"
+    private const val SELECT_BY_TIME_PERIOD = "SELECT * FROM userToDo WHERE creationDate BETWEEN :from AND :to"
+
   }
 
-  @Query(
-      SELECT_ALL)
+  @Query(SELECT_ALL)
   fun getAll(): List<UserToDo>
 
-  @Query(
-      SELECT_BY_ID)
+  @Query(SELECT_BY_ID)
   fun loadAllByIds(toDoIds: IntArray): List<UserToDo>
 
-  @Query(
-      SELECT_BY_NAME)
+  @Query(SELECT_BY_NAME)
   fun findByName(first: String, last: String): UserToDo
 
+  @Query(SELECT_BY_TIME_PERIOD)
+  fun findUsersBornBetweenDates(from: Date, to: Date): List<*>
   @Insert
   fun insertAll(vararg users: UserToDo)
 
